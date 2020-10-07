0ba8453346719930495ea22917231653f1566f31
Arina <32187787+theArina@users.noreply.github.com>
2020-10-07 12:48:57 +0300

    Add multiple tabs test

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,15 +1,26 @@
+const num = 5;
+let tabsCount = 0;
 let sumtime;
+const pagesTime = [];
 
 chrome.browserAction.onClicked.addListener(() => {
   sumtime = Date.now();
-  chrome.tabs.create({
-    active: false,
-    url: 'https://www.electronjs.org/',
-  }, ({ id }) => {
-    chrome.tabs.executeScript(id, { file: 'src/script.js', runAt: 'document_start' });
-  });
+  for (let count = 0; count < num; count++) {
+    chrome.tabs.create({
+      active: false,
+      url: 'https://www.electronjs.org/',
+    }, ({ id }) => {
+      chrome.tabs.executeScript(id, { file: 'src/script.js', runAt: 'document_start' });
+    });
+  }
 });
 
 chrome.runtime.onMessage.addListener(() => {
-  console.log('result', Date.now() - sumtime)
+  pagesTime.push(Date.now() - sumtime);
+  tabsCount++;
+  if (tabsCount === num) {
+    const summary = pagesTime.reduce((accumulator, currentValue) => accumulator + currentValue);
+    console.log('pagesTime', pagesTime);
+    console.log('average', summary / pagesTime.length);
+  }
 });
